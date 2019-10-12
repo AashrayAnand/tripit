9e20f24bb7e82b7170461751b0a30a1346bbc912
Alex <alwong34@uw.edu>
2019-10-12 16:23:12 -0700

    fixed the create trip test

--- trip/trip.go
+++ trip/trip.go
@@ -1,14 +1,11 @@
 package trip
 
-import {
+import (
 	"fmt"
 	"net/http"
 
-	"github.com/AashrayAnand/tripit/driver"
-	"github.com/AashrayAnand/tripit/models"
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
-}
+)
 
 // trip routing group should include all necessary functionality to
 
@@ -19,12 +16,12 @@ import {
 // Content-Type: json
 // POST form parameters will be in following form
 // user=<username>&pass=<pass>
-// check if querying trip collection for a given user 
+// check if querying trip collection for a given user
 // name returns an existing trip, if not, return "no trips"
 func Create(c *gin.Context) {
 	// get trip data from POST form
 	trip := c.PostForm("trip") // [{}, {}, ...]
-	res := fmt.Print(trip)
-	c.JSON(301, gin.H{"message": res, "status": http.OK})
+	res := fmt.Sprint(trip)
+	c.JSON(301, gin.H{"message": res, "status": http.StatusOK})
 	return
 }
