d5bb8fb5bb8aeb311e6d404ce7da80d95bebf9de
Aashray <aashray@uw.edu>
2019-10-13 22:42:12 -0700

    last ditch cors shit

--- server.go
+++ server.go
@@ -7,8 +7,8 @@ import (
 	"github.com/AashrayAnand/tripit/session"
 	"github.com/AashrayAnand/tripit/trip"
 	"github.com/AashrayAnand/tripit/user"
-	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -16,8 +16,8 @@ func main() {
 	_, _ = session.Client.SetNX("x", "y", 60*time.Second).Result()
 	val, err := session.Client.Get("x").Result()
 	if err != nil {
-	   fmt.Println("error", err.Error())
-	   return
+		fmt.Println("error", err.Error())
+		return
 	}
 	fmt.Println(val)
 
@@ -26,7 +26,13 @@ func main() {
 	user.AddUserRoutes(router) // add all routes in the /user groups
 	router.POST("/trip", trip.Create)
 
-	router.Use(cors.Default())
+	//router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST"},
+		AllowHeaders: []string{"Content-Type", "Origin"},
+		MaxAge:       12 * time.Hour,
+	}))
 	//router.Use(static.Serve("/", static.LocalFile("./views", true)))
 
 	router.Run(":3000") // listen and server 0.0.0.0:8080
