a5513b86e1c9cbb7f1941d263111d1dd9e318604
aashray <aashray@uw.edu>
2019-10-14 05:29:29 +0000

    cors enabled

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"github.com/AashrayAnand/tripit/trip"
 	"github.com/AashrayAnand/tripit/user"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -25,6 +26,7 @@ func main() {
 	user.AddUserRoutes(router) // add all routes in the /user groups
 	router.POST("/trip", trip.Create)
 
+	router.Use(cors.Default())
 	//router.Use(static.Serve("/", static.LocalFile("./views", true)))
 
 	router.Run(":3000") // listen and server 0.0.0.0:8080
